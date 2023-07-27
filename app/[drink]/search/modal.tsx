ef408a3f84cd162677548e8111266a903816bf57
DW Desktop <westerlund.da@gmail.com>
2023-07-27 22:56:51 +0200

    Animate search modal in and exit

--- app/[drink]/search/modal.tsx
+++ app/[drink]/search/modal.tsx
@@ -1,20 +1,30 @@
 import * as Portal from "@radix-ui/react-portal";
 import React from "react";
 import { Search } from "@/app/search/search";
+import { AnimatePresence, motion } from "framer-motion";
 
 function Modal({ open, close }: { open: boolean; close: () => void }) {
-  return open ? (
-    <Portal.Root>
-      <div
-        className="fixed inset-0 h-screen w-screen bg-black/40 px-10 pt-[60%] backdrop-blur-md"
-        onClick={(e) => {
-          if (e.target === e.currentTarget) close();
-        }}
-      >
-        <Search />
-      </div>
-    </Portal.Root>
-  ) : null;
+  return (
+    <AnimatePresence>
+      {open && (
+        <Portal.Root>
+          <motion.div
+            key="search-modal"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.15 }}
+            className="fixed inset-0 h-screen w-screen bg-black/40 px-10 pt-[60%] backdrop-blur-md"
+            onClick={(e) => {
+              if (e.target === e.currentTarget) close();
+            }}
+          >
+            <Search />
+          </motion.div>
+        </Portal.Root>
+      )}
+    </AnimatePresence>
+  );
 }
 
 export default Modal;
