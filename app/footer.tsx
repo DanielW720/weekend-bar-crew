f462b7501b64cc00268daf02acf6e2d12685aeab
Daniel Westerlund <westerlund.da@gmail.com>
2024-01-10 18:18:59 +0100

    Footer to its own component

---
+++ app/footer.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="z-10 flex w-full items-center justify-evenly border-t-2 border-beige bg-[#121212] px-6 py-2">
+      <div
+        className={`w-fit text-center text-[0.65rem] font-extrabold leading-[0.8rem] tracking-[0.2rem] text-beige`}
+      >
+        <h3>
+          Weekend
+          <br />
+          Bar
+          <br />
+          Crew
+        </h3>
+      </div>
+      <ul className="text-xs text-white">
+        <li className="inline-block">
+          <Link href={"/info"}>About</Link>
+        </li>
+        <li className="mx-4 inline-block">
+          <Link href={"/info"}>Contact</Link>
+        </li>
+        <li className="inline-block">
+          <Link href={"#"}>Instagram</Link>
+        </li>
+      </ul>
+    </footer>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import "./globals.css";
 import { Header } from "./header";
 import { inknut_antiqua } from "./lib/globals/fonts";
-import { BackdropImage } from "./backdropImage";
+import Footer from "./footer";
 
 export const metadata = {
   title: "Weekend Bar Crew",
@@ -20,30 +20,7 @@ export default function layout({ children }: { children: React.ReactNode }) {
           <Header />
           <main>{children}</main>
         </div>
-        <footer className="z-10 flex w-full items-center justify-evenly border-t-2 border-beige bg-[#121212] px-6 py-2">
-          <div
-            className={`w-fit text-center text-[0.65rem] font-extrabold leading-[0.8rem] tracking-[0.2rem] text-beige`}
-          >
-            <h3>
-              Weekend
-              <br />
-              Bar
-              <br />
-              Crew
-            </h3>
-          </div>
-          <ul className="text-xs text-white">
-            <li className="inline-block">
-              <Link href={"/about"}>About</Link>
-            </li>
-            <li className="mx-4 inline-block">
-              <Link href={"#"}>Contact</Link>
-            </li>
-            <li className="inline-block">
-              <Link href={"#"}>Instagram</Link>
-            </li>
-          </ul>
-        </footer>
+        <Footer />
       </body>
     </html>
   );
