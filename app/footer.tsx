7889fce6f1fa636e7b47503b280490543b857a2c
Daniel Westerlund <westerlund.da@gmail.com>
2024-01-14 17:58:02 +0100

    Footer responsive

--- app/footer.tsx
+++ app/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="z-10 flex w-full items-center justify-evenly border-t-2 border-beige bg-[#121212] px-6 py-2">
+    <footer className="z-10 flex w-full items-center justify-evenly border-t-2 border-beige bg-[#121212] px-6 py-2 sm:justify-center">
       <div
         className={`w-fit text-center text-[0.65rem] font-extrabold leading-[0.8rem] tracking-[0.2rem] text-beige`}
       >
@@ -15,7 +15,7 @@ export default function Footer() {
           Crew
         </h3>
       </div>
-      <ul className="text-xs text-white">
+      <ul className="text-xs text-white sm:ml-20">
         <li className="inline-block">
           <Link href={"/info"}>About</Link>
         </li>
