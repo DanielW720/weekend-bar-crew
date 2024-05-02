d0a88765da3ab1eb9794832d940588dbfe63b6e8
Daniel <westerlund.da@gmail.com>
2024-05-02 21:34:37 +0200

    Lang select in suspense boundary

--- app/[lang]/layout/header/header.tsx
+++ app/[lang]/layout/header/header.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
 import HeaderLogo from "./headerLogo";
 import LanguageSelection from "./languageSelection";
 
 export default async function Header({ slogan }: { slogan: string }) {
   return (
     <header className="group sticky top-0 z-[100] flex h-headerMobile w-full flex-row items-center justify-start border-b-4 border-beige bg-headerTransparent text-beige backdrop-blur-sm xxs:justify-center">
-      <LanguageSelection />
+      <Suspense fallback={<SearchFallback />}>
+        <LanguageSelection />
+      </Suspense>
       <div className="flex h-full flex-row items-end gap-x-4 pb-2 group-data-[scrolling=true]:scale-y-[0.925] group-data-[scrolling=true]:pb-1 xxs:gap-x-8 sm:gap-x-16 ">
         <HeaderLogo />
         <p className="w-[150px] overflow-hidden text-[0.575rem] leading-4 tracking-widest md:mb-2 md:w-[200px] md:text-[0.7rem] md:leading-5">
@@ -14,4 +16,9 @@ export default async function Header({ slogan }: { slogan: string }) {
       </div>
     </header>
   );
+}
+
+// TODO: Update fallback component
+function SearchFallback() {
+  return <div>Placeholder for Search</div>;
 }
