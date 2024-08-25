144fc6e7939a0acb546c8028ff3131c42163f1d2
Daniel <westerlund.da@gmail.com>
2024-08-25 18:53:41 +0200

    Instagram page is a work in progress

--- app/[lang]/info/instagram/page.tsx
+++ app/[lang]/info/instagram/page.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 function Page() {
   return (
-    <div className="mt-10 bg-blue-400">
-      The Instagram page is a work in progress. Almost.
+    <div className="mt-10 bg-blue-900 px-2">
+      <p className="text-center text-beige">
+        The Instagram page is a work in progress. Almost. ⚒️
+      </p>
     </div>
   );
 }
